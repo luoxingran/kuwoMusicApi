42053080b9cea8796c35f4575632c5ae9965c3df
lin <83531505+luoxingran@users.noreply.github.com>
2024-06-30 22:28:22 +0800

    Update playUrl.ts

--- app/service/playUrl.ts
+++ app/service/playUrl.ts
@@ -1,7 +1,7 @@
 const BaseService = require('./BaseService')
 
 export default class PlayUrl extends BaseService {
-  async getPlayUrl (mid, type = 'music', br = '128kmp3') {
-    return this.commonRequest(`http://www.kuwo.cn/api/v1/www/music/playUrl?mid=${mid}&type=${type}&httpsStatus=1&plat=web_www&from=&br=${br}`)
+  async getPlayUrl (rid, format = 'mp3') {
+    return this.commonRequest(`http://mobi.kuwo.cn/mobi.s?f=web&source=kwplayer_ar_4.4.2.7_B_nuoweida_vh.apk&type=convert_url2&rid=${rid}&priority=bitrate&format=${format}`)
   }
 }
